d6f91c58aa49d435b734b27192bf3c2a6abc78b5
Jeffrey Pratt <jeff@metronomix.com>
2013-02-18 09:32:39 -0700

    Removed the -f param, updated help and README

--- README.md
+++ README.md
@@ -6,20 +6,20 @@ Warp is a command-line warp speed calculator. Because you need one.
 
 Invoke it thus:
 
-    > warp -f 3
+    > warp 3
     > 27
 
 You can give it a distance in light-years, which will cause it to give the
 travel time in years:
 
-    > warp -f 5 -d 4.3
+    > warp 5 -d 4.3
     > 0.0344
 
 Warp supports both TOS and TNG warp speed scales:
 
-    > warp -f 5
+    > warp 5
     > 125
-    > warp -f 5 -t
+    > warp 5 -t
     > 213.747
 
 There you go.
@@ -28,12 +28,12 @@ There you go.
 
 You can use the output of warp in other programs. For example:
 
-    > units -qsv1 "`bin/warp -f 5` c" 'm/s'
+    > units -qsv1 "`bin/warp 5` c" 'm/s'
     > 125 c = 3.7474057e+10 m/s
 
 Or this:
 
-    > units -qsv1 "`bin/warp -t -f 5 -d 4.3` yr" 'day'
+    > units -qsv1 "`bin/warp -t 5 -d 4.3` yr" 'day'
     > 0.0201172 yr = 7.3476504 day
     
 ## Installation
--- src/main.c
+++ src/main.c
@@ -5,36 +5,57 @@
 
 #include "warp.h"
 
+void show_help(const char const *progname);
+
 int main(const int argc, char const **argv) {
   bool has_distance = false;
-  bool has_factor = false;
   bool use_tng = false;
 
   double distance_ly = 1.0;
   double factor = 1.0;
 
-  for (int i = 1; i < argc; i++) {
-    if (strncmp(argv[i], "-d", 2) == 0) {
-      has_distance = true;
-      if (i + 1 < argc)
-	distance_ly = strtod(argv[++i], NULL);
-    }
-
-    if (strncmp(argv[i], "-f", 2) == 0) {
-      has_factor = true;
-      if (i + 1 < argc)
-	factor = strtod(argv[++i], NULL);
-    }
-
-    if (strncmp(argv[i], "-t", 2) == 0)
-      use_tng = true;
-  }
-
-  if (!has_factor) {
+  if (argc < 2) {
     printf("Warp factor required.\n");
-    return 1;
+    return 0;
   }
 
+  if (strncmp(argv[1], "-h", 2) == 0
+      || strncmp(argv[1], "--help", 6) == 0)
+    {
+      show_help(argv[0]);
+      return 0;
+    }
+
+  factor = strtod(argv[1], NULL);
+
+  for (int i = 2; i < argc; i++) {
+    if (strncmp(argv[i], "-d", 2) == 0
+	|| strncmp(argv[i], "--distance", 10) == 0
+	|| strncmp(argv[i], "--dist", 6) == 0)
+      {
+	has_distance = true;
+	if (i + 1 < argc)
+	  distance_ly = strtod(argv[++i], NULL);
+	else {
+	  printf("Distance required.\n");
+	  return 1;
+	}
+      }
+
+    if (strncmp(argv[i], "-t", 2) == 0
+	|| strncmp(argv[i], "--tng", 5) == 0)
+      {
+	use_tng = true;
+      }
+
+    if (strncmp(argv[i], "-h", 2) == 0
+	|| strncmp(argv[i], "--help", 6) == 0)
+      {
+	show_help(argv[0]);
+	return 0;
+      }
+  }
+  
   double velocity = use_tng ? tng_warp(factor) : tos_warp(factor);
 
   if (has_distance) {
@@ -45,4 +66,12 @@ int main(const int argc, char const **argv) {
   }
 
   return 0;
+}
+
+void show_help(const char const *progname) {
+  printf("Usage: %s factor [options]\n", progname);
+  printf("Options:\n");
+  printf("  --distance, --dist, -d  Distance (in light-years)\n");
+  printf("  --tng, -t               Use TNG warp speed formula\n");
+  printf("  --help, -h              Show this message and exit\n");
 }
